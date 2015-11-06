b1bab0d47c0abfa8cecd51cc0464521704da71d4
fuchongjun <578140014@qq.com>
2015-11-07 02:59:18 +0800

    Initial Commit

---
+++ NoReadNameTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NoReadNameTest
+//
+//  Created by 付崇军 on 15/11/7.
+//  Copyright © 2015年 付崇军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NoReadNameTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NoReadNameTest
+//
+//  Created by 付崇军 on 15/11/7.
+//  Copyright © 2015年 付崇军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
